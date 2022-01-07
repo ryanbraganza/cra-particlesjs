2e8b2d7ccd9ac694884d3375ef0482c1365ea6f6
Ryan Braganza <ryan.braganza@gmail.com>
2022-01-08 10:24:08 +1100

    particles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'particles.js';
+
+const ParticlesJS = window.particlesJS;
+
+ParticlesJS.load('particles-js', '/assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="">
         <a
           className="App-link"
           href="https://reactjs.org"
